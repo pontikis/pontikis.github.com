740a7d177d2516a4488b9e29ac9fbff627acbbb8
Christos Pontikis <pontikis@gmail.com>
2013-10-19 18:23:50 +0300

    interface improvements

---
+++ index.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    $("#switch_jfr_prev").click(function() {
+        $("#jfr_prev").toggle();
+    });
+
+    $("#switch_jdr_prev").click(function() {
+        $("#jdr_prev").toggle();
+    });
+
+});
